ac325cd9e66c5b770eb061c7cae357a70d98cba6
LiBiYong <1255418023@qq.com>
2019-07-02 10:59:39 +0800

    init

---
+++ OpenSourceLibrary/OpenLibrary/OpenLibraryManager.h
@@ -0,0 +1,17 @@
+//
+//  OpenLibraryManager.h
+//  Example
+//
+//  Created by Bill on 2/7/2019.
+//  Copyright © 2019 Bill. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OpenLibraryManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ OpenSourceLibrary/OpenLibrary/OpenLibraryManager.m
@@ -0,0 +1,14 @@
+
+//
+//  OpenLibraryManager.m
+//  Example
+//
+//  Created by Bill on 2/7/2019.
+//  Copyright © 2019 Bill. All rights reserved.
+//
+
+#import "OpenLibraryManager.h"
+
+@implementation OpenLibraryManager
+
+@end
